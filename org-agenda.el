f3ffa1aa11460f40d11d2988e26d867c6e9b2dff
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-08-04 20:03:39 +0200

    Initial commit

---
+++ org-agenda.el
@@ -0,0 +1,44 @@
+;;; org-agenda.el ---
+
+;; Copyright (C) 2014 Grégoire Jadi
+
+;; Author: Grégoire Jadi <gregoire.jadi@gmail.com>
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(defcustom org-agenda-ignore-files nil
+  "The files not to be used for agenda display."
+  :group 'org-agenda
+  :type '(repeat :tag "List of files" file))
+
+(defun org-agenda-check-file ()
+  "Check whether the visited file is in `org-agenda-files'. If
+not, ask the user to add it. If the user refuse to add the file,
+it is added to `org-agenda-ignore-files'."
+  (let ((file (buffer-file-name)))
+    (unless (or (find file org-agenda-files)
+                (find file org-agenda-ignore-files))
+      (if (yes-or-no-p "Do you want to add this file to org-agenda-files? ")
+          (org-agenda-file-to-front)
+        (pushnew file org-agenda-ignore-files)))))
+
+(add-hook 'org-mode-hook 'org-agenda-check-file)
+
+(provide 'org-agenda)
+
+;;; org-agenda.el ends here
